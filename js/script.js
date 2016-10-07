b7256f002fab811a13f36bc9465b091a5096077d
Marc Tannous <tannous.marc@gmail.com>
2016-10-07 15:37:10 +0100

    Bugfixes
    Speed fix, wrong direction bug fixed. rotate(-3600deg)=rotate(-360deg)
    not yet fixed. Thank you CSS

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Initialize rotationDegrees()
+// Get current rotation angle
 (function($) {
   $.fn.rotationDegrees = function() {
     var matrix = this.css("-webkit-transform") ||
@@ -26,45 +26,44 @@ jQuery.fn.rotate = function(degrees) {
   });
   return $(this);
 };
-// End of rotationDegrees()
-// <---------------------->
-// Initialize random points on the circle, change # of digits left
-function initializare($param) {
+
+// Initialize random points on the circle, update # of digits
+function init($param) {
     var angle = Math.floor((Math.random() * 720) - 360);
     $("#circle2").rotate(angle);
     $("#container > p").html($param);
     if($param!=1)
-    $("#container > p").append("<br><h4>digits left</h4>");
-  else      $("#container > p").append("<br><h4>digit left</h4>");
+      $("#container > p").append("<br><h4>digits left</h4>");
+    else
+      $("#container > p").append("<br><h4>digit left</h4>");
   }
-  // End of initializare()
-  // <------------------->
+
 $(document).ready(function() {
-      // Click counter -> %2 == 0 is clockwise, %2 != 0 is counter-clockwise
+      // %2 == 0 is clockwise, else counter-clockwise
       var counter = 0;
-      // Digits counter, decreases to 0, where the player wins
-      var pointcounter = 5;
-      // Above mentioned initialization function takes # of digits left as a parameter, to display it
-      initializare(pointcounter);
-      // Let's store the randomly generated angle of the point in a variable
+      // # of digits, reach 0 => win
+      var digits = 5;
+      // display
+      init(digits);
+      // store the randomly generated angle of the point
       var angle = $("#circle2").rotationDegrees();
-      // Initial circle spin, on page load
+      // Initial circle spin on page load
       $("#circle").rotate(2880);
       $('#circle').click(function() {
         // Current rotation stored in a variable
         var unghi = $(this).rotationDegrees();
-        // If current rotation matches the random point rotation by a margin of +- 25, the player "hit" it and continues
+        // If current rotation matches the random point rotation by a margin of +- 2digits, the player "hit" it and continues
         if (unghi > angle - 25 && unghi < angle + 25) {
-          pointcounter--;
+          digits--;
           // If game over, hide the game, display end of game options
-          if (!pointcounter) {
+          if (!digits) {
             $("#circle").addClass("hidden");
             $("#circle2").addClass("hidden");
             $("#container > p").addClass("hidden");
             $("#retry2").removeClass("hidden");
           }
           // Else, add another point and remember its new angle of rotation
-          else initializare(pointcounter);
+          else init(digits);
           angle = $("#circle2").rotationDegrees();
         }
         // Else, the player "missed" and is brought to end of game options
@@ -76,18 +75,18 @@ $(document).ready(function() {
         }
         // No of clicks ++
         counter++;
-        // Orientation based on click parity
+        // spin based on click parity
         if (counter % 2) {
-          $(this).rotate(-1800);
-        } else $(this).rotate(1800);
+          $(this).rotate(-2880);
+        } else $(this).rotate(2160);
       });
       $('#retry').click(function() {
           $("#circle").removeClass("hidden");
           $("#circle2").removeClass("hidden");
           $("#container > p").removeClass("hidden");
           $("#retry").addClass("hidden");
-          pointcounter=5;
-          initializare(pointcounter);
+          digits=5;
+          init(digits);
           angle = $("#circle2").rotationDegrees();
           $("#circle").rotate(2440);
           counter=0;
@@ -96,9 +95,8 @@ $(document).ready(function() {
        $("#retry2").addClass("hidden");   $("#circle").removeClass("hidden");
           $("#circle2").removeClass("hidden");
           $("#container > p").removeClass("hidden");
-
-          pointcounter=5;
-          initializare(pointcounter);
+          digits=5;
+          init(digits);
           angle = $("#circle2").rotationDegrees();
           $("#circle").rotate(2440);
           counter=0;
